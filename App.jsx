c12a1dec4c996dd2c5d29de64b6ef1960dd35ad8
Sourav Karanth <151653711+KARSRV@users.noreply.github.com>
2024-10-03 19:01:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Box from "./components/Box";
+import Forecast from "./components/Forecast";
+const App = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <Box />
+    </div>
+  );
+};
+
+export default App;
